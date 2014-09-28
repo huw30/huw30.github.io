9aca479c0397be720041b13cbef311c0301c5fe3
huw30 <huan0924@gmail.com>
2014-09-28 17:27:48 -0400

    Minor change to contact js

--- js/email.js
+++ js/email.js
@@ -1,4 +1,29 @@
-
-// $('#form').submit(function(event) {
-//   alert('hey');
-// });
+$('#form').submit(function(event) {
+  var email = $(this).serializeArray();
+  event.preventDefault();
+   $.ajax({
+  type: 'POST',
+  url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+  data: {
+    'key': 'yzN1YsFPNFR7Ic7-8QcxOw',
+    'message': {
+      'from_email': email[1].value,
+      'to': [
+          {
+            'email': 'huan0924@gmail.com',
+            'name': 'huan',
+            'type': 'to'
+          }
+        ],
+      'autotext': 'true',
+      'subject': email[2].value,
+      'html': email[3].value
+    }
+  }
+  }).done(function(response) {
+    $('#tip-message').html('Message sent successfully!');
+    console.log(response);
+    setTimeout(function()
+     {$('#tip-message').html('');}, 5000);
+  });
+});
